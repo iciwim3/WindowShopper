9d7f748da6d37fa51234abca319c3e4700dc5323
Sain-R Edwards Jr <SKE@Sain-Rs-iMac-5.local>
2017-12-16 10:33:20 -0800

    First Commit

---
+++ window-shpper/window-shpper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shpper
+//
+//  Created by Sain-R Edwards Jr. on 12/16/17.
+//  Copyright © 2017 Appybuildmore Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
