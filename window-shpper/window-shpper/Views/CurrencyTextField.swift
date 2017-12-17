e446d61f6c7889af43250f90fa608404e3ebd9fb
Sain-R Edwards Jr <SKE@Sain-Rs-iMac-5.local>
2017-12-17 10:42:07 -0800

    Created custom label in CurrencyTextField class with currency locale function

--- window-shpper/window-shpper/Views/CurrencyTextField.swift
+++ window-shpper/window-shpper/Views/CurrencyTextField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable // Allows the IB to update with changes you make in code.
 class CurrencyTextField: UITextField {
     
+    // Only use if it is not going to be empty. Will cause performance issues!
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.7049711045)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     // You need this function for @IBDesignable to work
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -25,6 +41,8 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2533835827)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
