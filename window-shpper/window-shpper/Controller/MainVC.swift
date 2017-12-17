7396fa4b6a70cf53f0209632be1c7f828b015b2f
Sain-R Edwards Jr <SKE@Sain-Rs-iMac-5.local>
2017-12-17 09:26:10 -0800

    Added more UI elements to IB, created IBActions and IBOutlets to the VC, added the logic for the UI to work properly to the users input

--- window-shpper/window-shpper/Controller/MainVC.swift
+++ window-shpper/window-shpper/Controller/MainVC.swift
@@ -12,6 +12,8 @@ class MainVC: UIViewController {
     
     @IBOutlet weak var wageTxt: CurrencyTextField!
     @IBOutlet weak var priceTxt: CurrencyTextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +27,28 @@ class MainVC: UIViewController {
         
         wageTxt.inputAccessoryView = calcButton
         priceTxt.inputAccessoryView = calcButton
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
 
     @objc func calculate() {
-      
+        if let wageText = wageTxt.text, let priceText = priceTxt.text {
+            if let wage = Double(wageText), let price = Double(priceText) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
-
+    
+    @IBAction func clearCalcTapped(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
 }
 
